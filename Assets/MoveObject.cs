73629b035a35fe242c339c70e19d4c9c841d149e
fras0261 <fras0261@algonquinlive.com>
2016-01-25 17:51:51 -0500

    Initial Commitment.

---
+++ Assets/MoveObject.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveObject : MonoBehaviour {
+
+    public float xLimit;
+    public float yLimit;
+    public float speed;
+
+    private bool xMovingAway = true; 
+    private bool yMovingAway = true;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+        if (xMovingAway == true)
+        {
+            if (yMovingAway == true)
+            {
+                transform.localPosition += new Vector3(speed, speed, 0);
+
+                if (transform.localPosition.y >= yLimit )
+                    yMovingAway = false;
+            }
+                
+            else
+            {
+                transform.localPosition += new Vector3(speed, -speed, 0);
+
+                if (transform.localPosition.y <= -yLimit)
+                    yMovingAway = true;
+            }
+                
+            if (transform.localPosition.x >= xLimit || transform.localPosition.x <= -xLimit)
+                xMovingAway = false;
+            
+        }
+        else if (xMovingAway == false)
+        {
+            if (yMovingAway == true)
+            {
+                transform.localPosition += new Vector3(-speed, speed, 0);
+
+                if (transform.localPosition.y >= yLimit || transform.localPosition.y <= -yLimit)
+                    yMovingAway = false;
+            }
+
+            else
+            {
+                transform.localPosition += new Vector3(-speed, -speed, 0);
+               
+                if (transform.localPosition.y <= -yLimit)
+                    yMovingAway = true;
+            }
+
+            if (transform.localPosition.x <= -xLimit)
+                xMovingAway = true;
+        }
+       
+    }
+}
---
+++ Assets/SpawnObject.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnObject : MonoBehaviour {
+
+    public float respawnDistance;
+    public GameObject preFabToSpawn;
+
+    private Vector3 spawnerLocation;
+    private Vector3 currentLocation;
+
+    private float distance;
+    GameObject kefkaClone;
+
+    // Use this for initialization
+    void Start () {
+        spawnerLocation = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        distance = Vector3.Distance(transform.position, spawnerLocation);
+        
+        if (distance > respawnDistance)
+        {
+            kefkaClone = (GameObject)Instantiate(preFabToSpawn, spawnerLocation, Quaternion.identity);
+            kefkaClone.name = "Kefka Clone";
+            spawnerLocation = transform.position;
+        }
+
+
+	}
+}
